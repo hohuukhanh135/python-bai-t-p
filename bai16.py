853ce5be34254eba6942d16802bf503ca7d1e066
hohuukhanh135 <hohuukhanh135@gmail.com>
2026-03-20 09:36:31 +0700

    Update bai16.py

--- bai16.py
+++ bai16.py
@@ -1,5 +1,4 @@
 def zeller(day, month, year):
-    # Nếu là tháng 1,2 thì chuyển thành 13,14 của năm trước
     if month < 3:
         month += 12
         year -= 1
@@ -19,23 +18,19 @@ def days_in_month(month, year):
         return 30
     return 31
 
-# Nhập năm
 year = int(input("Nhập năm (>1582): "))
 
 for month in range(1, 13):
     print(f"\nTháng {month}")
     print("S  M  T  W  T  F  S")
 
-    # Tính thứ của ngày đầu tháng
     first = zeller(1, month, year)
     pos = (first + 6) % 7  # Đổi về: 0 = CN
 
     days = days_in_month(month, year)
 
-    # In khoảng trắng đầu
     print("   " * pos, end="")
 
-    # In ngày
     for d in range(1, days + 1):
         print(f"{d:2}", end=" ")
         if (pos + d) % 7 == 0:
